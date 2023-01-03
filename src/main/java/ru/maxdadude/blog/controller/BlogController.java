91bbc233df2cf084c7a3c063b4d38252cc864d85
theBestWasabi <openpw@icloud.com>
2023-01-03 10:07:15 +0300

    articles 0.0.2b

--- src/main/java/ru/maxdadude/blog/controller/BlogController.java
+++ src/main/java/ru/maxdadude/blog/controller/BlogController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.maxdadude.blog.model.Post;
 import ru.maxdadude.blog.repository.PostRepository;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -36,5 +40,17 @@ public class BlogController {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogRead(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-read";
     }
 }
